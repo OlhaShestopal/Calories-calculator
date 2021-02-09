6db8bf6e4eb2c9a224d1a6115161e9d421a64ea5
Olha <you@example.com>
2021-02-09 23:26:10 +0200

    product params

--- src/state/products/products.js
+++ src/state/products/products.js
@@ -37,38 +37,17 @@ store.on('products/save', (state, payload) => {
   const listItem = {...state.products.ProductProperties};
   listItem.productWeight = productWeight;
   const locationProducts = state.eatings.locationType;
-  console.log(locationProducts)
-  switch(locationProducts){
-    case 'breakfast':
-      return{
-        products: {
-          ...state.products,
-            breakfast:[...state.products.breakfast, listItem],
-          
+  
+  listItem.calories = (productWeight * listItem.calories)/100;
+  listItem.fat_total_g = (productWeight * listItem.fat_total_g)/100;
+  listItem.protein_g = (productWeight * listItem.protein_g)/100;
+  listItem.carbohydrates_total_g = (productWeight * listItem.carbohydrates_total_g)/100;
+  return{
+      products: {
+        ...state.products,
+          [locationProducts]:[...state.products[locationProducts], listItem],
         }
-      }
-      case 'lunch':
-        return{
-          products: {
-            ...state.products,
-              lunch:[...state.products.lunch, listItem],
-          }
-        }
-        case 'supper':
-          return{
-            products: {
-              ...state.products,
-                supper:[...state.products.supper, listItem],
-            }
-          }
-          case 'snack':
-            return{
-              products: {
-                ...state.products,
-                  snack:[...state.products.snack, listItem],
-                }
-              }
-            }
+  }
   }
 )
 
